e222bf1cfc1acf63e26f4ac663afeabfbd259d6a
MAHADI HASAN AFRIDI <79738922+MAHADI-143@users.noreply.github.com>
2022-07-30 00:20:30 +0600

    Update MAHADI-8.py

--- MAHADI-8.py
+++ MAHADI-8.py
@@ -7,8 +7,5 @@ except:
 import requests
 bit = platform.architecture()[0]
 if bit == '64bit':
-    from mahadi8 import Subscraption
-    Subscraption()
-elif bit == '32bit':
-    from mahadi import menu
+    from mahadi8 import menu
     menu()
